6704d0e13eb64e3df12a0e718fdb906dabd9c0ee
Ben <bssipes@gmail.com>
2014-07-17 20:03:29 -0500

    1.3
    Began work on MainActivity.java Several TODO items listed for future
    updates.
    Updated README file with Changelog

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 CutieDateHelper by Benjamin Sipes
 ===============
 This project is a Java Android Application. The intended use is to help QuikTrip employees in confirming the correct julian dates to place on products, such as grill food, breakfast sandwiches, and frozen that need thaw/stale dates.
+
+Changelog:
+1.1) Begin planning / toying with layout for Settings menu
+
+1.11) Moved 1.0 MainActivity into OldStyle, made OldStyle the default loaded
+activity. No (intended) changes to how the app currently looks, though
+Settings is now accessible.
+
+1.12) Settings now loads properly and remembers checkbox / default store
+settings. MainActivity is not loading afterwards, getting Null Pointer
+Exception upon clicking Save and Submit button.
+
+1.2) 1.1 (saving/retrieving data) seems to be functional now, moving on to
+passing the data to MainActivity. As of current, still crashing.
+
+1.21) Fixed MainActivity crash. Now properly loads and displays the searched
+Store#. Currently working on settings up all the needed strings for the
+Gen2 layout.
+TODO: Research how to make swipe left/right transitions
+
+1.22) Finished setting up the MainActivity xml layout
+
+1.23) Forgot to update a copy/paste job in the Main xml.
+
+1.24) Began work on MainActivity.java Several TODO items listed for future updates.
+Updated README file with Changelog
--- src/com/bsipes/cutiedatehelper/MainActivity.java
+++ src/com/bsipes/cutiedatehelper/MainActivity.java
@@ -1,17 +1,63 @@
 package com.bsipes.cutiedatehelper;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity 
+{
 
-	public String store; //set in onCreate
-	public boolean gen3Mode; //set in onCreate
+	public String store; // set in onCreate
+	public boolean gen3Mode; // set in onCreate
 	TextView displayStoreNumber_TV;
-	
-	protected void onCreate(Bundle savedInstanceState) 
+
+	public void main() 
 	{
+		GregorianCalendar gc = (GregorianCalendar) GregorianCalendar.getInstance();
+
+		TextView gregorian_TV = (TextView) findViewById(R.id.gregorian_TV);
+		gregorian_TV.setText((gc.get(Calendar.MONTH) + 1) + "/" + gc.get(Calendar.DAY_OF_MONTH) + "/" + gc.get(Calendar.YEAR));
+
+		TextView julian_TV = (TextView) findViewById(R.id.julian_TV);
+		int julian = gc.get(Calendar.DAY_OF_YEAR);
+		julian_TV.setText(String.valueOf(julian));
+
+		int totalDays = 365;
+		if (gc.isLeapYear(Calendar.YEAR)) {
+			totalDays += 1;
+		setgen2Dates(store, julian);
+		}
+	}
+	private void setgen2Dates(String store, int julian) {
+		//declare all the textviews
+		TextView gen2_chilicheese_date = (TextView) findViewById(R.id.gen2_chilicheese_date);
+		TextView gen2_wallcoolerpizzas_date = (TextView) findViewById(R.id.gen2_wallcoolerpizzas_date);
+		TextView gen2_grillfood_date = (TextView) findViewById(R.id.gen2_grillfood_date);
+		TextView gen2_hotzis_date = (TextView) findViewById(R.id.gen2_hotzis_date);
+		TextView gen2_wallcoolerburritos_date = (TextView) findViewById(R.id.gen2_wallcoolerburritos_date);
+		TextView gen2_nemo_date = (TextView) findViewById(R.id.gen2_nemo_date);
+//TODO:lookup the division number
+		int d = findDivision(store); //this is just a stub at the moment
+//TODO:lookup the dates (dates? dating patterns?) for that division
+		//If I store the dating patterns, how should I do that? Some categories have 2 dates, some have 3.
+//TODO:set the text for the textview based on this data
+//		gen2_chilicheese_date.setText("");
+//		gen2_wallcoolerpizzas_date.setText("");
+//		gen2_grillfood_date.setText("");
+//		gen2_hotzis_date.setText("");
+//		gen2_wallcoolerburritos_date.setText("");
+//		gen2_nemo_date.setText("");
+		
+	}
+
+	private int findDivision(String store) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		Bundle bundle = getIntent().getExtras();
@@ -19,5 +65,5 @@ public class MainActivity extends Activity {
 		displayStoreNumber_TV = (TextView) findViewById(R.id.DisplayStoreNumber_TV);
 		displayStoreNumber_TV.setText(store);
 		gen3Mode = bundle.getBoolean("gen3Mode");
-	}//end onCreate
+	}// end onCreate
 }
